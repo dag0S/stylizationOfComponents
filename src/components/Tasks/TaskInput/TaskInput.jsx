0ce3c670d3acccc1e448b17cca30c2a636220ab1
Gosudarev Danila <danidagosudarev@gmail.com>
2023-11-04 23:15:17 +0300

    61-65

---
+++ src/components/Tasks/TaskInput/TaskInput.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './TaskInput.css';
+
+import styled from 'styled-components';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#d44242' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+
+  &.invalid input {
+    border: 1px red solid;
+    background: #d44242;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
+
+const TaskInput = props => {
+  const [inputText, setinputText] = useState('');
+  const [isInputValid, setIsInputValid] = useState(true);
+
+  const taskInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsInputValid(true);
+    }
+    setinputText(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
+    props.onAddTask(inputText);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* <FormControl className={!isInputValid && 'invalid'}> */}
+      <FormControl invalid={!isInputValid}>
+        <label>Задачи</label>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </FormControl>
+      <Button type="submit">Добавить Задачу</Button>
+    </form>
+  );
+};
+
+export default TaskInput;
